41f1fa75037fe548b4dd43684027c1eefc9e56af
Alan Reji <90831246+AlanReji010@users.noreply.github.com>
2021-09-18 10:48:19 +0530

    Update test.java

--- Sample/src/test.java
+++ Sample/src/test.java
@@ -2,6 +2,7 @@ public class test{
 
 	//Test changes to show that the file changes can cause conflict
 	public static void main(String args[]) {
+		// New additional changes
 		int a=29;
 		int b=48;
 		int c=a+b;
