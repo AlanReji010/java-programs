488f428484f6f9f53f4e47d96b1d1201fe58caf5
Alan Reji <alanreji.2000@gmail.com>
2021-09-20 15:42:56 +0530

    2nd commit

---
+++ Sample/src/Switchtest.java
@@ -0,0 +1,18 @@
+
+public class Switchtest {
+	public static void main(String args[]) {
+		int a=15;
+		switch(a) {
+		case 10 : System.out.println("10");
+				  break;
+		case 15 : System.out.println("15");
+				  break;
+		case 20 : System.out.println("20");
+				  break;
+	    default : System.out.println("NOT 10,15 or 20");
+	    		  break;
+	    		  
+		}
+		
+	}
+}
